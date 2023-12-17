f4faff7dee01e97f9b0734cd434346f18cd29837
Alexander Mikhaliou <a.m.mihalev@gmail.com>
2023-12-17 15:18:50 +0300

    Second Commit at Change -. Test changin https to ssh

--- Homework #3/ViewController.swift
+++ Homework #3/ViewController.swift
@@ -14,8 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-//        Hello world
-//        test system
+//        Hello world  test system
+//        Check changin https to ssh
         
         let test = ""
         
